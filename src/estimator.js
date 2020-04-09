650b7ebd5498a02b5f528496cd371768ef7f29be
Emmanuel Kiprono Davidson <kipronofb@gmail.com>
2020-04-09 08:30:29 +0300

    reported cases

--- src/estimator.js
+++ src/estimator.js
@@ -1,12 +1,3 @@
-const covid19ImpactEstimator = (data) => {
-    return {
-        data, 
-        impact: {
-            currentlyInfected:data.reportedCases * 10,
-    }, 
-    severeImpact:{
-        currentlyInfected: data.reportedCases*50,
-    }}
-};
+const covid19ImpactEstimator = (data) => data;
 
 export default covid19ImpactEstimator;
