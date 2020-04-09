69498e06be4dddd028b16dda5736d410911d5c7e
Emmanuel Kiprono Davidson <kipronofb@gmail.com>
2020-04-09 18:49:35 +0300

    initial commit

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,31 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+  const response = {
+    data,
+    estimate: {
+      impact: {
+        currentlyInfected: '',
+        infectionsByRequestedTime: '',
+        severeCasesByRequestedTime: '',
+        hospitalBedsByRequestedTime: '',
+        casesForICUByRequestedTime: '',
+        casesForVentilatorsByRequestedTime: '',
+        dollarsInFlight: ''
+
+      },
+      severeImpact: {
+        currentlyInfected: '',
+        infectionsByRequestedTime: '',
+        severeCasesByRequestedTime: '',
+        hospitalBedsByRequestedTime: '',
+        casesForICUByRequestedTime: '',
+        casesForVentilatorsByRequestedTime: '',
+        dollarsInFlight: ''
+
+      }
+    }
+
+  };
+  return response;
+};
 
 export default covid19ImpactEstimator;
