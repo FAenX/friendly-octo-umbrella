969615d8f848310ab2404950faebb532272891a9
Emmanuel Kiprono Davidson <kipronofb@gmail.com>
2020-04-10 11:49:51 +0300

    currently infected

--- src/estimator.js
+++ src/estimator.js
@@ -1,19 +1,43 @@
+
+// impact: infectionsByRequestedTime
+const infectionsByRequestedTimeImpact = (data) => {
+  const factor = Math.floor(data.timeToElapse / 3);
+  return data.impact.currentlyInfected * 2 ^ factor;
+};
+
+
+// impact: severeCasesByRequestedTime
+const severeCasesByRequestedTimeImpact = () => {};
+
+// impact: hospitalBedsByRequestedTime
+const hospitalBedsByRequestedTimeImpact = () => {};
+
+// impact: casesForICUByRequestedTime
+const casesForICUByRequestedTimeImpact = () => {};
+
+// impact: casesForVentilatorsByRequestedTime
+const casesForVentilatorsByRequestedTimeImpact = () => {};
+
+// impact: dollarsInFlight
+const dollarsInFlightImpact = () => {};
+
+
 const covid19ImpactEstimator = (data) => {
   const response = {
     data,
     estimate: {
       impact: {
-        currentlyInfected: '',
-        infectionsByRequestedTime: '',
-        severeCasesByRequestedTime: '',
-        hospitalBedsByRequestedTime: '',
-        casesForICUByRequestedTime: '',
-        casesForVentilatorsByRequestedTime: '',
-        dollarsInFlight: ''
+        currentlyInfected: data.reportedCases * 10,
+        infectionsByRequestedTime: infectionsByRequestedTimeImpact(data),
+        severeCasesByRequestedTime: severeCasesByRequestedTimeImpact(data),
+        hospitalBedsByRequestedTime: hospitalBedsByRequestedTimeImpact(data),
+        casesForICUByRequestedTime: casesForICUByRequestedTimeImpact(data),
+        casesForVentilatorsByRequestedTime: casesForVentilatorsByRequestedTimeImpact(data),
+        dollarsInFlight: dollarsInFlightImpact(data)
 
       },
       severeImpact: {
-        currentlyInfected: '',
+        currentlyInfected: data.reportedCases * 50,
         infectionsByRequestedTime: '',
         severeCasesByRequestedTime: '',
         hospitalBedsByRequestedTime: '',
