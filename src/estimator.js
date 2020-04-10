6bfdd7f2f00d5a59fa6816caf644de90f4b4342a
coderafiki <kipronofb@gmail.com>
2020-04-10 17:50:44 +0300

    challenge1

--- src/estimator.js
+++ src/estimator.js
@@ -1,25 +1,51 @@
+/* eslint-disable linebreak-style */
 // impact: currently Infected
 const currentlyInfectedImpact = (data) => data.reportedCases * 10;
+// severe impact
+const currentlyInfectedSevereImpact = (data) => data.reportedCases * 50;
+
 // impact: infectionsByRequestedTime
 const infectionsByRequestedTimeImpact = (data) => {
   const factor = Math.round(data.timeToElapse / 3);
   return currentlyInfectedImpact(data) * (2 ** factor);
 };
 
+// severe impact
+const infectionsByRequestedTimeSevereImpact = (data) => {
+  const factor = Math.round(data.timeToElapse / 3);
+  return currentlyInfectedSevereImpact(data) * (2 ** factor);
+};
+
+
 // impact: severeCasesByRequestedTime
-const severeCasesByRequestedTimeImpact = () => {};
+const severeCasesByRequestedTimeImpact = (data) => (15 / 100) * infectionsByRequestedTimeImpact(data);
+
+// severe impact
+const severeCasesByRequestedTimeSevereImpact = (data) => (15 / 100) * infectionsByRequestedTimeSevereImpact(data);
 
 // impact: hospitalBedsByRequestedTime
-const hospitalBedsByRequestedTimeImpact = () => {};
+const hospitalBedsByRequestedTimeImpact = (data) => (35 / 100) * data.totalHospitalBeds;
+
+// severe impact
+const hospitalBedsByRequestedTimeSevereImpact = () => {};
 
 // impact: casesForICUByRequestedTime
 const casesForICUByRequestedTimeImpact = () => {};
 
+// severe impact
+const casesForICUByRequestedTimeSevereImpact = () => {};
+
 // impact: casesForVentilatorsByRequestedTime
 const casesForVentilatorsByRequestedTimeImpact = () => {};
 
+// severe impact
+const casesForVentilatorsByRequestedTimeSevereImpact = () => {};
+
 // impact: dollarsInFlight
 const dollarsInFlightImpact = () => {};
+
+// severe impact
+const dollarsInFlightSevereImpact = () => {};
 
 
 const covid19ImpactEstimator = (data) => {
@@ -37,13 +63,13 @@ const covid19ImpactEstimator = (data) => {
 
       },
       severeImpact: {
-        currentlyInfected: data.reportedCases * 50,
-        infectionsByRequestedTime: '',
-        severeCasesByRequestedTime: '',
-        hospitalBedsByRequestedTime: '',
-        casesForICUByRequestedTime: '',
-        casesForVentilatorsByRequestedTime: '',
-        dollarsInFlight: ''
+        currentlyInfected: currentlyInfectedSevereImpact(data),
+        infectionsByRequestedTime: infectionsByRequestedTimeSevereImpact(data),
+        severeCasesByRequestedTime: severeCasesByRequestedTimeSevereImpact(data),
+        hospitalBedsByRequestedTime: hospitalBedsByRequestedTimeSevereImpact(data),
+        casesForICUByRequestedTime: casesForICUByRequestedTimeSevereImpact(data),
+        casesForVentilatorsByRequestedTime: casesForVentilatorsByRequestedTimeSevereImpact(data),
+        dollarsInFlight: dollarsInFlightSevereImpact(data)
 
       }
     }
