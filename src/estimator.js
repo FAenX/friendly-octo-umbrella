93284405b22de2d906f7c1e9c4464630a2f6ec08
coderafiki <kipronofb@gmail.com>
2020-04-10 18:07:06 +0300

    challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -6,13 +6,28 @@ const currentlyInfectedSevereImpact = (data) => data.reportedCases * 50;
 
 // impact: infectionsByRequestedTime
 const infectionsByRequestedTimeImpact = (data) => {
-  const factor = Math.round(data.timeToElapse / 3);
+  let factor;
+  if (data.periodType === 'days') {
+    factor = Math.floor(data.timeToElapse / 3);
+  } else if (data.periodType === 'weeks') {
+    factor = Math.floor((data.timeToElapse * 7) / 3);
+  } else if (data.periodType === 'months') {
+    factor = Math.floor((data.timeToElapse * 30) / 3);
+  }
+
   return currentlyInfectedImpact(data) * (2 ** factor);
 };
 
 // severe impact
 const infectionsByRequestedTimeSevereImpact = (data) => {
-  const factor = Math.round(data.timeToElapse / 3);
+  let factor;
+  if (data.periodType === 'days') {
+    factor = Math.floor(data.timeToElapse / 3);
+  } else if (data.periodType === 'weeks') {
+    factor = Math.floor((data.timeToElapse * 7) / 3);
+  } else if (data.periodType === 'months') {
+    factor = Math.floor((data.timeToElapse * 30) / 3);
+  }
   return currentlyInfectedSevereImpact(data) * (2 ** factor);
 };
 
